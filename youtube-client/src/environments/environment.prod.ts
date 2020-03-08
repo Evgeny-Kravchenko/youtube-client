b6f2c62531bf945ced7fff8e9b4282b456119dee
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-03-08 13:59:10 +0300

    fix: fix error with production version

--- youtube-client/src/environments/environment.prod.ts
+++ youtube-client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.type';
 
 export const environment: Environment = {
   production: true,
-  API_URL: `https://www.googleapis.com/youtube/v3/search`,
+  API_URL: `https://www.googleapis.com/youtube/v3`,
   API_KEY: 'AIzaSyDcdgDSc_NYZmBkoy1EfHw0fnZccpohlyU'
 };
